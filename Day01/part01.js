c1b13564ae92ea32e9572c6fb88be74e47274411
花间 <huajian@helijia.com>
2019-11-22 19:52:32 +0800

    feat: add solution of [Day 01 Part 01]

---
+++ Day01/part01.js
@@ -0,0 +1,12 @@
+import txt from './input.txt';
+
+var input = txt.split('');
+var res = input.reduce((acc, dir) => {dir === '(' ? ++acc : --acc}, 0);
+console.log('res:', res);
+
+/*
+输入：文本 txt、方向 dir
+输出：累计结果 acc
+思路：reduce((acc, dir) => {})
+控制台使用：var txt = document.querySelectorAll('pre')[0] 取到文本内容，打印 res 值为 280
+*/
