cead9b1d6b6b7c3362ece2213bd9843e1a658c99
花间 <huajian@helijia.com>
2019-11-25 22:51:03 +0800

    solve Day 02 Part 01

---
+++ Day02/part01/index.js
@@ -0,0 +1,23 @@
+import txt from './input.txt';
+
+var input = txt.slice(0, -1).split('\n'); // 这里有个坑，txt.split('\n').length = 1001，其中 txt.split('\n')[1000] = ''
+var res = input.reduce((totalArea, curStr) => {
+  var dims = curStr.split('x');
+  var lw = dims[0] * dims[1];
+  var wh = dims[1] * dims[2];
+  var lh = dims[0] * dims[2];
+
+  return totalArea + 2 * (lw + wh + lh) + Math.min(lw, wh, lh);
+  
+}, 0);
+
+console.log('res: ', res); // res: 1606483
+
+/*
+输入：文本 txt、面积计算公式 2 * (lw + wh + lh) + Math.min(lw, wh, lh)
+输出：总面积 totalArea
+思路：reduce((acc, cur) => {})
+控制台使用：var txt = document.querySelectorAll('pre')[0].innerText 取到文本内容，打印 res 值为 1606483
+*/
+
+/* 简单粗暴的方法写起来简单，但应该有针对单个面积的更好的计算方法，可以再想一下 */
