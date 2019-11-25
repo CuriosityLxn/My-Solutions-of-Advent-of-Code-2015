01211801068d867c7db785eec17ee78d81cb0ac9
花间 <huajian@helijia.com>
2019-11-26 00:37:35 +0800

    补充数组去重解法，去重需要重复遍历，不太棒的做法

--- Day03/part01/index.js
+++ Day03/part01/index.js
@@ -1,6 +1,5 @@
 import txt from './input.txt';
 
-var txt = document.querySelectorAll('pre')[0].innerText;
 var x = 0, y = 0, visited = [];
 var input = txt.split('');
 
@@ -18,6 +17,23 @@ console.log('res:', res); // res: 2572
 /*
 输入：文本 txt、方向 dir
 输出：访问过的值的个数
-思路：一开始想用二维数组，数组长度不固定，array.push() 不能避免重复的值，所以用了 obj 来做
+思路：一开始想用二维数组，数组长度不固定，array.push() 不能避免重复的值(去重需要额外判断，不如直接利用对象性质)，所以用了 obj 来做
 控制台使用：var txt = document.querySelectorAll('pre')[0].innerText 取到文本内容，打印 res 值为 2572
 */
+
+
+// 补 数组去重法
+
+var txt = document.querySelectorAll('pre')[0].innerText;
+var x = 0, y = 0, visited = [];
+var input = txt.split('');
+
+input.forEach(dir => {
+  dir === '^' && y++;
+  dir === '>' && x++;
+  dir === 'v' && y--;
+  dir === '<' && x--;
+  visited.push(`(${x}, ${y})`);
+})
+
+var res = visited.reduce((pre, acc) => pre.includes(acc) ? pre : [...pre, acc], []).length;
